8be853a8575463962ad48787cce218c2342fa293
Mike Lang <mike.lang1010@gmail.com>
2015-10-22 18:45:16 -0500

    I think if I make this a normal attribute it will override the default

--- nodejs/attributes/customize.rb
+++ nodejs/attributes/customize.rb
@@ -1,2 +1,2 @@
 # override the repo attribute so we get node 4.x
-default['nodejs']['repo']      = 'https://deb.nodesource.com/node_4.x'
+normal['nodejs']['repo']      = 'https://deb.nodesource.com/node_4.x'
