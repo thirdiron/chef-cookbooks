b661fc8daeb568389501e3a060f4d9d9fd490036
Mike Lang <mike.lang1010@gmail.com>
2015-10-22 15:17:19 -0500

    Used Berkshelf to initialize stuff

---
+++ Berksfile
@@ -0,0 +1,6 @@
+source "https://supermarket.chef.io"
+
+metadata
+
+cookbook 'nodejs', '~> 2.4.2'
+cookbook 'heroku-toolbelt', '~> 0.1.0'
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'livingroom'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures livingroom'
+long_description 'Installs/Configures livingroom'
+version          '0.1.0'
