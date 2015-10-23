578cbab17e51d1cd104e747a6f9debeea4d8a009
Mike Lang <mike.lang1010@gmail.com>
2015-10-22 20:49:16 -0500

    chef complained saying I need to configure this here

--- 3i-nodejs/metadata.rb
+++ 3i-nodejs/metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures 3i-nodejs'
 long_description 'Installs/Configures 3i-nodejs'
 version          '0.1.0'
+
+depends 'nodejs', '~> 2.4.2'
