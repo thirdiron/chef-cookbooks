1e703277c017d8eb051e59cb987ea197e8543521
Mike Lang <mike.lang1010@gmail.com>
2015-10-23 17:25:32 -0500

    Update dependency to old version that works with chef 11.10

--- livingroom_conf/metadata.rb
+++ livingroom_conf/metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures livingroom-conf'
 long_description 'Installs/Configures livingroom-conf'
 version          '0.1.0'
 
-depends 'rabbitmq', '~> 4.4.0'
+depends 'rabbitmq', '~> 3.3.0'
