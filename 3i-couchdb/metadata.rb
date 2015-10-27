d8b55f6a3579e35fef6441d722e3ae2190a57db6
Mike Lang <mike.lang1010@gmail.com>
2015-10-27 16:17:24 -0500

    Missing space causes errors

--- 3i-couchdb/metadata.rb
+++ 3i-couchdb/metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures 3i-couchdb'
 long_description 'Installs/Configures 3i-couchdb'
 version          '0.1.0'
 
-depends 'couchdb', '~>2.5.2'
+depends 'couchdb', '~> 2.5.2'
