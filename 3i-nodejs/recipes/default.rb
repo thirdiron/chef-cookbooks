8155548244a8359afa957ebd66388e834968be61
Mike Lang <mike.lang1010@gmail.com>
2015-10-27 23:07:54 -0500

    Create a link so the opsworks nodejs deploy cookbook finds node

--- 3i-nodejs/recipes/default.rb
+++ 3i-nodejs/recipes/default.rb
@@ -9,3 +9,11 @@
 # include the dependent nodejs recipe, but with our module's attributes 
 # so we fetch the appropriate version of nodeJS
 include_recipe 'nodejs::nodejs_from_package'
+
+# The opsworks nodejs recipe is really picky about where
+# nodejs is installed so we have to create a link from
+# where it expects the node executable to be to where
+# the nodejs cookbook installs it.
+link '/usr/local/bin/node' do
+  to '/usr/bin/node'
+end
