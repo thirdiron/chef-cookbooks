50e557078eafe9d069644798d7f36eda1bdd9014
Mike Lang <mike.lang1010@gmail.com>
2016-03-21 16:05:27 -0500

    Recipe that ought to generate PM application config JSON

---
+++ 3i-deploy/definitions/ti_opsworks_pm2_nodejs.rb
@@ -0,0 +1,34 @@
+define :ti_opsworks_pm2_nodejs do
+  deploy = params[:deploy_data]
+  application = params[:app]
+
+  node[:dependencies][:npms].each do |npm, version|
+    execute "/usr/local/bin/npm install #{npm}" do
+      cwd "#{deploy[:deploy_to]}/current"
+    end
+  end
+
+  # Not sure what opsworks does with this, but we'll preserve it
+  template "#{deploy[:deploy_to]}/shared/config/opsworks.js" do
+    cookbook 'opsworks_nodejs'
+    source 'opsworks.js.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(:database => deploy[:database], :memcached => deploy[:memcached], :layers => node[:opsworks][:layers])
+  end
+
+  template "#{deploy[:deploy_to]}/current/pm2-app.json" do
+    source 'pm2-application.json.erb'
+    cookbook '3i-deploy'
+    user deploy[:user]
+    group deploy[:group]
+    variables(
+      :application_name => application,
+      :cwd => "#{deploy[:deploy_to]}/current/housecleaning",
+      :script => "cron.js",
+      :environment => deploy[:environment_variables]
+    )
+  end
+  
+end 
---
+++ 3i-deploy/recipes/deploy-maintenance.rb
@@ -0,0 +1,29 @@
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  # This recipe is for opsworks applications
+  # of type 'other' that declare their
+  # OPSWORKS_APPLICATION_TYPE in their
+  # environment variables as "nodejs-PM2"
+  if deploy[:application_type] != 'other' || deploy['environment_variables']['OPSWORKS_APPLICATION_TYPE'] != 'nodejs-PM2'
+    Chef::Log.debug("Skipping 3i-deploy::deploy-maintenance for application #{application} as it is not a PM2 app")
+    next
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
+  ti_opsworks_pm2_nodejs do
+    deploy_data deploy
+    app application
+  end
+
+end
