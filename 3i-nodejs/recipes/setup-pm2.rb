5af2e5e70622263f516b7b389ef577c716b473b1
Mike Lang <mike.lang1010@gmail.com>
2016-03-21 12:35:20 -0500

    Run shell command to setup startup script

--- 3i-nodejs/recipes/setup-pm2.rb
+++ 3i-nodejs/recipes/setup-pm2.rb
@@ -2,3 +2,7 @@ nodejs_npm "pm2" do
   version "1.0.2"
 end
 
+execute "setup-pm2-startup-script" do
+  command 'env PATH=$PATH:/usr/bin pm2 startup ubuntu -u ubuntu --hp /home/ubuntu'
+  user 'root'
+end
