764c460e358a791b550b4163b227031fc6ad1ce5
Mike Lang <mike.lang1010@gmail.com>
2016-03-22 13:25:06 -0500

    Run command with HOME set to ubuntu's home
    This is where pm2 decides to put its logs cause idk

--- 3i-deploy/recipes/deploy-nodejs-PM2.rb
+++ 3i-deploy/recipes/deploy-nodejs-PM2.rb
@@ -40,7 +40,7 @@ node[:deploy].each do |application, deploy|
   end
 
   execute 'start_or_restart_cronjs' do
-    command "bash -c \"pm2 startOrRestart #{deploy[:deploy_to]}/current/pm2-app.json\""
+    command "env HOME=~ubuntu pm2 startOrRestart #{deploy[:deploy_to]}/current/pm2-app.json"
     user 'ubuntu'
   end
 
