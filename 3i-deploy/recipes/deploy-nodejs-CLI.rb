0aab00b67ded8b88bf5da0b94eec042ab58e776e
Mike Lang <mike.lang1010@gmail.com>
2016-04-04 14:06:54 -0500

    Need to actually create the configuration file

--- 3i-deploy/recipes/deploy-nodejs-CLI.rb
+++ 3i-deploy/recipes/deploy-nodejs-CLI.rb
@@ -30,5 +30,12 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
+  application_environment_file do
+    user deploy[:user]
+    group deploy[:group]
+    path ::File.join(deploy[:deploy_to], "shared")
+    environment_variables deploy[:environment_variables]
+  end
+
 end
 
