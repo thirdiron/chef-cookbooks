dfcdfa65b2cf88fcfbf55f5677359f2ed9d64f5f
Mike Lang <mike.lang1010@gmail.com>
2016-09-29 15:55:15 -0500

    Fixes to before rotate script
    Apparently logrotate passes the whole list of files that will be rotated

--- 3i-server-commons/files/default/beforeRotateLog.sh
+++ 3i-server-commons/files/default/beforeRotateLog.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# first argument - path to log file that's getting rotated
-# second argument - name of s3 bucket where log file should be archived
+# first argument - name of s3 bucket where log file should be archived
+# remaining arguments - paths to log file that are getting rotated
 
-datestamp=`date +%m_%d_%y_%H_%M`
-archivalname=$(basename $1)_$datestamp
-cp $1 /tmp/$archivalname
-gzip /tmp/$archivalname
+bucket=$1
+shift
+for filePath in "$@"
+do
+  datestamp=`date +%m_%d_%y_%H_%M`
+  archivalname=$(basename $filePath)_$datestamp
+  echo "First param: $1"
+  echo "Second param: $2"
+  echo "Third param: $3"
+  cp $filePath /tmp/$archivalname
+  gzip /tmp/$archivalname
 
-s3put $2 /tmp/${archivalname}.gz 
-
+  s3put $bucket /tmp/${archivalname}.gz
+done
