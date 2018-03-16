7e48eb77f663fa62c42bdcc50825fe7c5d0ff57c
Karl Becker <karl@karlbecker.com>
2018-03-16 14:02:28 -0500

    Connect PM2 to Keymetrics
    Using env variables for the two arguments - will they automatically be pulled in like this with the way I've set things up in opsworks?

--- 3i-nodejs/recipes/setup-pm2.rb
+++ 3i-nodejs/recipes/setup-pm2.rb
@@ -6,3 +6,8 @@ execute "setup-pm2-startup-script" do
   command 'env PATH=$PATH:/usr/bin pm2 startup ubuntu -u ubuntu --hp /home/ubuntu'
   user 'root'
 end
+
+execute "connect-pm2-to-keymetrics" do 
+  command 'pm2 link $KEYMETRICS_SECRET $KEYMETRICS_PUBLIC'
+  user 'root'
+end
