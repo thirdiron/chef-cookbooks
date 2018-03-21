502828d09d5a52cca8cb553d48db84595819bc78
Mike Lang <mike.lang1010@gmail.com>
2018-03-21 16:04:33 -0500

    Update to node 8 repo

--- 3i-nodejs/attributes/default.rb
+++ 3i-nodejs/attributes/default.rb
@@ -1,3 +1,3 @@
-# override the repo attribute so we get node 4.x
-default['nodejs']['repo']      = 'https://deb.nodesource.com/node_4.x'
+# override the repo attribute so we get node 8.x
+default['nodejs']['repo']      = 'https://deb.nodesource.com/node_8.x'
 
