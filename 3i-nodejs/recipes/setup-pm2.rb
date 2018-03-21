4f0903fcd451d7ad4899c713bad21601e935841a
Mike Lang <mike.lang1010@gmail.com>
2018-03-21 16:04:05 -0500

    ubuntu needs to have access to this to install modules

--- 3i-nodejs/recipes/setup-pm2.rb
+++ 3i-nodejs/recipes/setup-pm2.rb
@@ -2,6 +2,16 @@ nodejs_npm "pm2" do
   version "2.10.1"
 end
 
+# set the modules folder owner to ubuntu
+# so the ubuntu user can actually install modules
+# (like the server monitor module)
+directory '/home/ubuntu/.pm2/modules' do
+  owner 'ubuntu'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
 execute "setup-pm2-startup-script" do
   command 'env PATH=$PATH:/usr/bin pm2 startup -u ubuntu --hp /home/ubuntu'
   user 'root'
