420e83edfb45a5e9eacf62a4964a91fca520da10
Mike Lang <mike.lang1010@gmail.com>
2018-03-21 14:08:50 -0500

    Add keymetrics stuff to pm2 recipe

--- 3i-nodejs/recipes/setup-pm2.rb
+++ 3i-nodejs/recipes/setup-pm2.rb
@@ -6,3 +6,13 @@ execute "setup-pm2-startup-script" do
   command 'env PATH=$PATH:/usr/bin pm2 startup -u ubuntu --hp /home/ubuntu'
   user 'root'
 end
+
+execute "link-pm2-to-keymetrics" do
+  command 'pm2 link #{node["keymetrics"]["secret-key"]} #{node["keymetrics"]["public-key"]}'
+  user 'ubuntu'
+end
+
+execute "setup-keymetrics-server-monitoring" do
+  command 'pm2 install pm2-server-monit'
+  user 'ubuntu'
+end
