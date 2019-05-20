5c123adbd25d33a744d6ff1aa8d0b224f400554b
Keegan Jorgensen <keegan@thirdiron.com>
2019-05-20 16:05:55 -0500

    Add a recipe for aussie's rabbitMQ

---
+++ livingroom_conf/recipes/configure_rabbitMQ_aussie.rb
@@ -0,0 +1,72 @@
+
+include_recipe 'rabbitmq::mgmt_console'
+
+rabbitmq_vhost "aussie" do
+  action :add
+end
+
+rabbitmq_user "guest" do
+  action :delete
+end
+
+rabbitmq_user node['rabbitmq-settings']['admin-user'] do
+  password node['rabbitmq-settings']['admin-password']
+  action :add
+end
+
+rabbitmq_user node['rabbitmq-settings']['admin-user'] do
+  vhost "aussie"
+  permissions ".* .* .*"
+  action :set_permissions
+end
+
+rabbitmq_user node['rabbitmq-settings']['admin-user'] do
+  tag "administrator"
+  action :set_tags
+end
+
+# user for the aussie to use - no configure rights
+# read/write anywhere in the vhost
+rabbitmq_user 'aussie' do
+  password node['rabbitmq-settings']['aussie-password']
+  action :add
+end
+
+rabbitmq_user 'aussie' do
+  vhost "aussie"
+  permissions "^$ .* .*"
+  action :set_permissions
+end
+
+# user for the CMS to use - no configure rights
+# read/write anywhere in the vhost
+rabbitmq_user 'browzinecms' do
+  password node['rabbitmq-settings']['browzinecms-password']
+  action :add
+end
+
+rabbitmq_user 'browzinecms' do
+  vhost "aussie"
+  permissions "^$ .* .*"
+  action :set_permissions
+end
+
+# Need to mention rsyslog so template updates
+# can notify with the :restart command
+service 'rsyslog' do
+  :nothing
+end
+
+# Configure logs to go to logentries
+template "/etc/rsyslog.d/70-rabbitmq.conf" do
+  source '70-rabbitmq.conf.erb'
+  cookbook 'livingroom_conf'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  variables(
+    :hostname => node["opsworks"]["instance"]["hostname"],
+    :logentries_token => node["rabbitmq-settings"]["logentries-token"]
+  )
+  notifies :restart, "service[rsyslog]", :immediately
+end
